498cba880d8067d88c6f158e3d31061853f0939e
Mahyar Karimnejad <mahyar.gamer.tbz@gmail.com>
2022-11-30 21:58:53 +0330

    Demo Of The Resume Ali dvlprr

--- src/Components/MiniComponents/Video Background/VideoBakground.js
+++ src/Components/MiniComponents/Video Background/VideoBakground.js
@@ -2,9 +2,7 @@ import React from "react";
 import ComingSoon from '../../Videos/1111.mp4'
 function VideoBakground() {
     return (
-        <div className="div-video">
-            <video src={ComingSoon} controls={false} autoPlay loop muted />
-        </div>
+        <video src={ComingSoon} controls={false} autoPlay loop muted />
     );
 }
 
