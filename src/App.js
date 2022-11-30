46f3c491e9f4cbbf92b8a37b9073c9ae0a3ffeec
Mahyar Karimnejad <mahyar.gamer.tbz@gmail.com>
2022-11-30 19:44:00 +0330

    ljkedfjkwr

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import UserMain from "./Components/UserMain";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
   return (
     <div className="loader-span-sty">
-      <div className="main-container">
-        <UserMain />
-      </div>
+      {loading ? (
+        <RingLoader
+          size={150}
+          color={"rgba(255, 255, 255, 1)"}
+          loading={loading}
+        />
+      ) : (
+        <div className="main-container">
+          <UserMain />
+        </div>
+      )
+      }
     </div>
   );
 }
